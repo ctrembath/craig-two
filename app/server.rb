7eb927385af7f27d98715f5aefb0b9a126b9e45d
ctrembath <claretrembath@ymail.com>
2015-12-22 19:33:48 +0000

    working git

--- app/server.rb
+++ app/server.rb
@@ -9,9 +9,9 @@ get '/' do
 	# S3 settings
 	s3_connect
 	all_files
-	puts @my_bucket
-	@file = get_asset('TankShort.mp4')
-	@key = @file.key
+	title_name
+
+	# @key = @file.key
 	erb :index
 end
 
@@ -23,6 +23,13 @@ def s3_connect
 	:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 )
+end
+
+def title_name
+	@tank = get_asset('TankShort.mp4')
+	@cliff = get_asset('cliff.mp4')
+	@risenfall = get_asset('risefall.mp4')
+	@chill = get_asset('calmcraig.png')
 end
 
 def all_files
